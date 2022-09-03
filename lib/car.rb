56ef6a1a8af4ff7a9346d3908e3172e40d5552b6
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-09-03 17:35:33 -0600

    first iteration completed

---
+++ lib/car.rb
@@ -0,0 +1,20 @@
+class Car
+  attr_reader :name, :monthly_payment, :loan_length
+ def initialize(name, monthly_payment, loan_length)
+  @name = name
+  @monthly_payment = monthly_payment
+  @loan_length = loan_length
+ end
+
+ def make
+   @name.split.first 
+ end
+
+ def model
+  @name.split[1]
+ end
+
+ def total_cost
+  @monthly_payment * @loan_length 
+ end
+end
